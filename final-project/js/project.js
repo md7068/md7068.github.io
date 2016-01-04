93605ff9ad6abd8cf7fb8630025cddc9a29523a0
md7068 <dwcatech@gmail.com>
2016-01-04 18:14:42 -0500

    1-4-16
    What Cheese Are You?

--- final-project/js/project.js
+++ final-project/js/project.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	$('img').hide()
+	$('#fancy').hide()
 	$('#q1').hide()
 	$('#q2').hide()
 	$('#q3').hide()
@@ -19,16 +21,123 @@ $(document).ready(function() {
 	$('#text8').hide()
 	$('#text9').hide()
 	$('#text10').hide()
-	$('button').hide()
-	$('h1').hover(function(){
-		$('#cheese').show(500);
-	})
+	$('#b1').hide()
+	$('#b2').hide()
+	$('#b3').hide()
+	$('#b4').hide()
+	$('#b5').hide()
+	$('#b6').hide()
+	$('#b7').hide()
+	$('#b8').hide()
+	$('#b9').hide()
+	$('#b10').hide()
+	
+
+	$('cheese').show()
+	
 
 	$('#cheese').click(function(){
-	$('#q1').show(500);
-	$('#text1').show(500);
-	$('#b1').show(500);
+		$('#q1').show(500);
+		$('#text1').show(500);
+		$('#b1').show(500);
 
+	$('#b1').click(function(){
+		$('#q1').hide(500)
+		$('#text1').hide(500)
+		$('#b1').hide(500)
+		$('#cheese').hide(500)
+		$('#q2').show(500);
+		$('#text2').show(500);
+		$('#b2').show(500);
+
+		$('#b2').click(function(){
+		$('#q2').hide(500)
+		$('#text2').hide(500)
+		$('#b2').hide(500)
+		$('#cheese').hide(500)
+		$('#q3').show(500);
+		$('#text3').show(500);
+		$('#b3').show(500);
+
+	$('#b3').click(function(){
+		 $('#q3').hide(500)
+		 $('#text3').hide(500)
+		 $('#b3').hide(500)
+		 $('#cheese').hide(500)
+		 $('#q4').show(500);
+		 $('#text4').show(500);
+		 $('#b4').show(500);
+
+	$('#b4').click(function(){
+	    $('#q4').hide(500)
+		$('#text4').hide(500)
+		$('#b4').hide(500)
+		$('#cheese').hide(500)
+		$('#q5').show(500);
+		$('#text5').show(500);
+		$('#b5').show(500);
+
+	$('#b5').click(function(){
+		$('#q5').hide(500)
+		$('#text5').hide(500)
+		$('#b5').hide(500)
+		$('#cheese').hide(500)
+		$('#q6').show(500);
+		$('#text6').show(500);
+		$('#b6').show(500);
+
+	$('#b6').click(function(){
+		$('#q6').hide(500)
+		$('#text6').hide(500)
+		$('#b6').hide(500)
+		$('#cheese').hide(500)
+		$('#q7').show(500);
+		$('#text7').show(500);
+		$('#b7').show(500);
+		
+	$('#b7').click(function(){
+		$('#q7').hide(500)
+		$('#text7').hide(500)
+		$('#b7').hide(500)
+		$('#cheese').hide(500)
+		$('#q8').show(500);
+		$('#text8').show(500);
+		$('#b8').show(500);
+
+	$('#b8').click(function(){
+		$('#q8').hide(500)
+		$('#text8').hide(500)
+		$('#b8').hide(500)
+		$('#cheese').hide(500)
+		$('#q9').show(500)
+		$('#text9').show(500)
+		$('#b9').show(500)
+
+	$('#b9').click(function(){
+		$('#q9').hide(500)
+		$('#text9').hide(500)
+		$('#b9').hide(500)
+		$('#cheese').hide(500)
+		$('#q10').show(500)
+		$('#text10').show(500)
+		$('#b10').show(500)
+	$('#b10').click(function(){
+		$('#q10').hide(500)
+		$('#text10').hide(500)
+		$('#b10').hide(500)
+		$('img').show(500)
+		$('#fancy').fadeIn(600)
+	})
+	})
+	})
+	})
+	})
+
+	})
+	})
+	})
+	})
+	})
 	})
 	
 	})
